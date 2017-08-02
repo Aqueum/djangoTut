977412c3ab6ad759ae247f81294f2696cdf425b6
Aqueum <martin@aqueum.com>
2017-08-02 19:23:45 +0100

    Make views.py Hello World

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request): return HttpResponse("Hello, world. You're at the polls index.")`
+
+
+add new polls / urls.py with content: `from django.conf.urls import url
+from . import views
+
+urlpatterns = [url(r'^$', views.index, name='index'), ]
