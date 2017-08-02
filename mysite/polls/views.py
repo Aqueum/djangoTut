beb4c2f33273e10435f3978f2230385f1f7af94d
Aqueum <martin@aqueum.com>
2017-08-02 20:10:10 +0100

    Add Hello World in /polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,5 @@
 from django.http import HttpResponse
 
 
-def index(request): return HttpResponse("Hello, world. You're at the polls index.")`
-
-
-add new polls / urls.py with content: `from django.conf.urls import url
-from . import views
-
-urlpatterns = [url(r'^$', views.index, name='index'), ]
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
