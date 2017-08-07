d2558c1ec360a3d0662747bfd899597d7ef55e0c
Aqueum <martin@aqueum.com>
2017-08-07 10:56:23 +0100

    Add published recently to list view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -15,7 +15,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 
 admin.site.register(Question, QuestionAdmin)
